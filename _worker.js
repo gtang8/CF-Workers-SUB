03acab1dbfc4c52f054472038af781a9effb0a94
gtang8 <74369627+gtang8@users.noreply.github.com>
2024-06-19 13:32:24 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ let ChatID =''; //可以为空，或者@userinfobot中获取，/start
 let TG = 0; //小白勿动， 开发者专用，1 为推送所有的访问信息，0 为不推送订阅转换后端的访问信息与异常访问
 let FileName = 'CF-Workers-SUB';
 let SUBUpdateTime = 6; //自定义订阅更新时间，单位小时
-let total = 99;//PB
+let total = 99;//TB
 let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
@@ -43,8 +43,8 @@ export default {
 		const fakeToken = await MD5MD5(`${mytoken}${timeTemp}`);
 		//console.log(`${fakeUserID}\n${fakeHostName}`); // 打印fakeID
 
-		let UD = Math.floor(((timestamp - Date.now())/timestamp * 99 * 1099511627776 * 1024)/2);
-		total = total * 1099511627776 * 1024;
+		let UD = Math.floor(((timestamp - Date.now())/timestamp * total * 1099511627776 )/2);
+		total = total * 1099511627776 ;
 		let expire= Math.floor(timestamp / 1000) ;
 		SUBUpdateTime = env.SUBUPTIME || SUBUpdateTime;
 
@@ -121,7 +121,7 @@ export default {
 								if (content.includes('dns') && content.includes('proxies') && content.includes('proxy-groups')) {
 									//console.log("clashsub: " + url);
 									订阅转换URL += "|" + url;
-								} else if  (content.includes('dns') && content.includes('outbounds') && content.includes('inbounds')){
+								} else if (content.includes('dns') && content.includes('outbounds') && content.includes('inbounds')){
 									//console.log("singboxsub: " + url);
 									订阅转换URL += "|" + url;
 								} else {
@@ -135,14 +135,14 @@ export default {
 						}
 					})
 				));	
-				//console.log(responses);
+			
 				for (const response of responses) {
-					if (response.status === 'fulfilled') {
-						const content = await response.value;
+					if (response.status === 'fulfilled' && response.value) {
+						const content = response.value;
 						req_data += base64Decode(content) + '\n';
 					}
 				}
-
+			
 			} catch (error) {
 				//console.error(error);
 			} finally {
@@ -162,7 +162,7 @@ export default {
 			
 			const base64Data = btoa(result);
 
-			if (订阅格式 == 'base64'){
+			if (订阅格式 == 'base64' || token == fakeToken){
 				return new Response(base64Data ,{
 					headers: { 
 						"content-type": "text/plain; charset=utf-8",
